1c14b403195ec2152ba63fa8b3f9c3e3f8a1a402
sshah <sshah@42six.com>
2013-08-27 23:35:10 -0400

    Adding StreamEvent

---
+++ frack-api/src/main/thrift/StreamEvent.thrift
@@ -0,0 +1,8 @@
+namespace java ezbake.frack
+
+struct StreamEvent {
+	1: required string dateTime; // The date time for the stream event captured in ISO 8601 YYYY-MM-DDThh:mm:ssTZD (see: http://www.w3.org/TR/NOTE-datetime)
+	2: optional string authorization; // The authorization of the particular event
+	3: optional string origin; // Where the event came from
+	4: required binary content; // The content of the particular event
+}
