bcaca249d602e641040722a3aa01bceee7d05a8e
Nyla Khalil <nylakhalil@users.noreply.github.com>
2013-09-12 10:18:48 -0400

    Update ConfigSettings.java

--- frack-api/src/main/java/ezbake/frack/core/Config/ConfigSettings.java
+++ frack-api/src/main/java/ezbake/frack/core/Config/ConfigSettings.java
@@ -9,9 +9,9 @@ public class ConfigSettings  implements Serializable {
 	Map<String, Object> settings = Maps.newHashMap();
 	
 	/**
-    * Define a new configuration map.
-    * WARNING: If configuration already exists, it will be over written. 
-    * @param map <code>Map<String,Object></code> The configuration of this component. This configuration is used to manage processes.
+    	* Define a new configuration map.
+    	* WARNING: If configuration already exists, it will be over written. 
+    	* @param map <code>Map<String,Object></code> The configuration of this component. This configuration is used to manage processes.
 	*/
 	public void addSettings(Map<String,Object> map) {
 		if (!settings.isEmpty()){
@@ -21,27 +21,27 @@ public class ConfigSettings  implements Serializable {
 	}
 	
 	/**
-    * Define a new configuration item.
-    * WARNING: If configuration already exists, it will be over written. 
-    * @param name <code>String</code> The key/name of the setting.
-    * @param value <code>Object</code> The value of the setting.
+    	* Define a new configuration item.
+    	* WARNING: If configuration already exists, it will be over written. 
+    	* @param name <code>String</code> The key/name of the setting.
+    	* @param value <code>Object</code> The value of the setting.
 	*/
 	public void addSetting(String name, Object value) {
 		settings.put(name, value);
 	}
 	
 	/**
-    * Retrieve configuration map.
-    * @return map <code>Map<String,Object></code> The configuration of this component. This configuration is used to manage processes.
+    	* Retrieve configuration map.
+    	* @return map <code>Map<String,Object></code> The configuration of this component. This configuration is used to manage processes.
 	*/
 	public Map<String, Object> getSettings() {
 		return settings;
 	}
 	
 	/**
-    * Retrieve a configuration item.
-    * @param name <code>String</code> The key/name of the setting.
-    * @return value <code>Object</code> The value of the setting.
+    	* Retrieve a configuration item.
+    	* @param name <code>String</code> The key/name of the setting.
+    	* @return value <code>Object</code> The value of the setting.
 	*/
 	public Object getSetting(String name) {
 		Object ret = null;
